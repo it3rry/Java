2f950b7abb049cddb05c3b2bb345d8c1d52024da
pf201811281558819 <acchn@live.com>
2019-08-29 16:37:27 +0800

    增加了收银系统

--- HelloJava/src/terry/HelloJava.java
+++ HelloJava/src/terry/HelloJava.java
@@ -1,7 +1,25 @@
 package terry;
 
+import java.nio.channels.ScatteringByteChannel;
+import java.util.Scanner;
+
 public class HelloJava {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+
+        System.out.print("你好，欢迎进入管理系统,请输入收款金额：");
+        Scanner in = new Scanner(System.in);
+        int price ;
+        price = in.nextInt();
+//        System.out.print("您输入的金额是：");
+//        System.out.print(price);
+
+        //应收金额，这里应是一个系统传进来的值，先用一个输入值代替
+        Scanner money_result = new Scanner(System.in);
+        System.out.print("请输入应收的费用:");
+        int money;
+        money = money_result.nextInt();
+
+        System.out.print("用找钱：");
+        System.out.println(price + "-" + money + "=" + (price - money));
     }
 }
