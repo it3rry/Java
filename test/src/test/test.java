7ddeda9303afd6b013108261ca318cdc092a05df
pf201811281558819 <acchn@live.com>
2019-12-18 22:44:36 +0800

    exercise

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -1,5 +1,6 @@
 package test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -12,15 +13,28 @@ import java.util.HashSet;
  */
 public class test {
     public static void main(String[] args) {
-        HashSet<String> a = new HashSet<String>();
-        a.add("first");
-        a.add("second");
-        a.add("first");
-        System.out.println(a);
-        System.out.println("----------");
-        HashMap<Integer,String> hm = new HashMap<Integer, String>();
-        hm.put(1,"现在是1");
-        hm.put(2,"现在是2");
-        System.out.println(hm);
+//        HashSet<String> a = new HashSet<String>();
+//        a.add("first");
+//        a.add("second");
+//        a.add("first");
+//        System.out.println(a);
+//        System.out.println("----------");
+//        HashMap<Integer,String> hm = new HashMap<Integer, String>();
+//        hm.put(1,"现在是1");
+//        hm.put(2,"现在是2");
+//        System.out.println(hm);
+        ArrayList al = new ArrayList();
+        al.add("111");
+
+        ArrayList al2 = new ArrayList();
+        al2.add("zhaotianyue");
+        al.addAll(al2);
+
+        al.add(0,"a");
+//        System.out.println(al.get(0));
+
+        for (int i = 0;i < al.size();i++){
+            System.out.println(al.get(i));
+        }
     }
 }
