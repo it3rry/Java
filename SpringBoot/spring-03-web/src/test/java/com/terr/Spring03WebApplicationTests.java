dbd52b4f6758c4c340cf13c3a41faba50c03f531
pf201811281558819 <acchn@live.com>
2019-12-23 22:32:18 +0800

    clear up

---
+++ SpringBoot/spring-03-web/src/test/java/com/terr/Spring03WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring03WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
