a81b181b201f2fb77edebb93b7c0d3c26874a935
pf201811281558819 <acchn@live.com>
2020-01-08 23:13:27 +0800

    测试Spring创建实例是否走了默认构造方法.

---
+++ Spring/Spring-03/src/test/java/MyTest.java
@@ -0,0 +1,11 @@
+import com.terr.pojo.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    public static void main(String[] args) {
+        ApplicationContext content = new ClassPathXmlApplicationContext("beans.xml");
+        User user = (User) content.getBean("user");
+
+    }
+}
