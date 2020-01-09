f519ac3701d09fc6dab9114e5d5b1432b851cd37
pf201811281558819 <acchn@live.com>
2020-01-09 20:59:52 +0800

    p命名空间注入

--- Spring/Spring-04/src/test/java/MyTest.java
+++ Spring/Spring-04/src/test/java/MyTest.java
@@ -1,4 +1,6 @@
 import com.terr.pojo.Student;
+import com.terr.pojo.User;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -8,6 +10,16 @@ public class MyTest {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Student student = (Student) context.getBean("student");
         System.out.println(student.getName());
+
+    }
+
+    @Test
+    public void test2(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("userbeans.xml");
+        User user = context.getBean("user",User.class);
+
+        System.out.println(user);
+
 
     }
 }
