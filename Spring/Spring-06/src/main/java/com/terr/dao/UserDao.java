f0a1ac04bd75a477c4eaaae5f24e8ea31c143f99
pf201811281558819 <acchn@live.com>
2020-01-12 17:42:54 +0800

    衍生注解

---
+++ Spring/Spring-06/src/main/java/com/terr/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.terr.dao;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDao {
+}
---
+++ Spring/Spring-06/src/main/java/com/terr/service/UserService.java
@@ -0,0 +1,8 @@
+package com.terr.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
