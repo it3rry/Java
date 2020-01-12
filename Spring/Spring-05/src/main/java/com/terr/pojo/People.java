c49eb67711e39bcde2b1068c36213a66a5cdc05b
pf201811281558819 <acchn@live.com>
2020-01-12 16:28:17 +0800

    使用@Autowired实现自动装配

--- Spring/Spring-05/src/main/java/com/terr/pojo/People.java
+++ Spring/Spring-05/src/main/java/com/terr/pojo/People.java
@@ -1,7 +1,12 @@
 package com.terr.pojo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class People {
+    //如果显示定义了require的值为false,说明这个对象可以为null,否则不允许为空
+    @Autowired(required = false)
     private Cat cat;
+    @Autowired
     private Dog dog;
     private String name;
 
