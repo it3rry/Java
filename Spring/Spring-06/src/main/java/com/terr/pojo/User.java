ec7a3ae6b9a19f244e74b61b96de4d5e086361d3
pf201811281558819 <acchn@live.com>
2020-01-12 17:18:26 +0800

    属性注入

--- Spring/Spring-06/src/main/java/com/terr/pojo/User.java
+++ Spring/Spring-06/src/main/java/com/terr/pojo/User.java
@@ -1,10 +1,13 @@
 package com.terr.pojo;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 //等价与在xml中注册bean
 @Component
 public class User {
-    public  String name = "iTerry";
+
+    @Value("iTerry")
+    public  String name;
 }
