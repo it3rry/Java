867e40614294c25925d5377e5f03a6ca5211b223
pf201811281558819 <acchn@live.com>
2020-01-12 17:15:55 +0800

    使用注解进行开发.

---
+++ Spring/Spring-06/src/main/java/com/terr/pojo/User.java
@@ -0,0 +1,10 @@
+package com.terr.pojo;
+
+
+import org.springframework.stereotype.Component;
+
+//等价与在xml中注册bean
+@Component
+public class User {
+    public  String name = "iTerry";
+}
---
+++ Spring/Spring-06/src/test/java/MyTest.java
@@ -0,0 +1,12 @@
+import com.terr.pojo.User;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+         User user = context.getBean("user",User.class);
+        System.out.println(user.name);
+    }
+}
