ef212bb35ae80908beff12fbf74c7858f7213cc7
pf201811281558819 <acchn@live.com>
2020-01-13 20:27:46 +0800

    不使用xml,是用config类来完成配置文件的功能.

--- Spring/Spring-07/src/main/java/com/terr/config/Myconfig.java
+++ Spring/Spring-07/src/main/java/com/terr/config/Myconfig.java
@@ -5,9 +5,11 @@ import com.terr.pojo.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration  //component
 public class Myconfig {
 
+    //这个注解代表注册了Bean,相当与 xml中的 <bean .../>
+    //方法的名字相当与id, 返回值相当于class属性
     @Bean
     public User getUser(){
         return new User();
