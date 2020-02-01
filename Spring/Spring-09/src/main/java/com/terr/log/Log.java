2574fad1056c3a1891c259c3238c6cd321ee8e6a
pf201811281558819 <acchn@live.com>
2020-02-01 10:37:20 +0800

    AOP实现方式一

--- Spring/Spring-09/src/main/java/com/terr/log/Log.java
+++ Spring/Spring-09/src/main/java/com/terr/log/Log.java
@@ -6,7 +6,10 @@ import java.lang.reflect.Method;
 
 public class Log implements MethodBeforeAdvice {
 
-    public void before(Method method, Object[] objects, Object o) throws Throwable {
-
+    //method :要执行的目标对象的方法
+    //args :参数
+    //target: 目标
+    public void before(Method method, Object[] args, Object target) throws Throwable {
+        System.out.println(target.getClass().getName()+"的"+method.getName()+"被执行了");
     }
 }
---
+++ Spring/Spring-09/src/test/java/MyTest.java
@@ -0,0 +1,14 @@
+import com.terr.service.UserService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.naming.Context;
+
+public class MyTest {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserService userService = context.getBean("userService", UserService.class);
+
+        userService.add();
+    }
+}
