dbdd09084db932acbfd0cfa1459051d8d5b0a8cc
pf201811281558819 <acchn@live.com>
2020-02-05 23:28:48 +0800

    搞清注解

--- SpringBoot/src/main/java/com/terr/springboot/controller/HelloController.java
+++ SpringBoot/src/main/java/com/terr/springboot/controller/HelloController.java
@@ -2,20 +2,17 @@ package com.terr.springboot.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 //类上的标签(注解)是标志
-@Controller
-@RequestMapping("/hello")
+@RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    @ResponseBody
+    //接口: localhost:8081/hello
+    @RequestMapping("/hello")
     public String hello(){
-        return "hello";
+        //调用业务,接受前端的参数
+        return "hello,world";
     }
 }
