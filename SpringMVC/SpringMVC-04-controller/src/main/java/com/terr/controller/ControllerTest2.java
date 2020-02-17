d1fe5859b9f55a21c111b64acb5ae463c1135333
pf201811281558819 <acchn@live.com>
2020-02-17 11:19:27 +0800

    test2==>使用注解方式

---
+++ SpringMVC/SpringMVC-04-controller/src/main/java/com/terr/controller/ControllerTest2.java
@@ -0,0 +1,16 @@
+package com.terr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller//代表这个类被spring接管了
+public class ControllerTest2 {
+
+    @RequestMapping("/t2")
+    public String test1(Model model){
+
+        model.addAttribute("msg","ControllerTest2");
+        return "test";
+    }
+}
