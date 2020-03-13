b98a19d508235ce69a29801e902f83d753b54b4d
pf201811281558819 <acchn@live.com>
2020-03-13 22:37:15 +0800

    data相关

---
+++ SpringBoot/spring-04-data/src/test/java/com/terr/Spring04DataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring04DataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
