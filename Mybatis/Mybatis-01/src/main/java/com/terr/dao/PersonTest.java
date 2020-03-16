bfbf9f340c34890667f396f65f76b0db6771f9bd
pf201811281558819 <acchn@live.com>
2020-03-16 22:43:36 +0800

    mybatis

---
+++ Mybatis/Mybatis-01/src/main/java/com/terr/dao/PersonTest.java
@@ -0,0 +1,12 @@
+package com.terr.dao;
+
+
+public class PersonTest {
+    public static void main(String[] args) {
+        Person p1 = new Person("Terry",18);
+
+        System.out.println(p1);
+    }
+
+
+}
